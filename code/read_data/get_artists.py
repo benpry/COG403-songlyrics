c5fa7a850759b939152476f334cc65fd68d37a89
Ria Prakash <riaprakash.1999@gmail.com>
2021-03-24 13:24:56 -0400

    Collected solo artists

---
+++ code/read_data/get_artists.py
@@ -0,0 +1,54 @@
+"""
+This file reads data from the musicxmatch dataset in the file "mxm_779k_matches.txt",
+determines which are solo artists using US baby names from "NationalNames.csv",
+and saves the names of solo artists in the file
+"""
+
+import pandas as pd
+import numpy as np
+import csv
+
+NAMES_PATH = "../../data/NationalNames.csv"
+MXM_PATH = "../../data/mxm_779k_matches.txt"
+OUTPUT_PATH = "../../data/processed/artists.csv"
+
+if __name__ == "__main__":
+
+    # Get names from US national names dataset
+    data = pd.read_csv(NAMES_PATH, dtype = {'Count': np.int32})
+    names = data['Name'].tolist()
+    names.remove('The')
+    
+    # Get all artists in mxm dataset
+    all_artists = []
+    file = open(MXM_PATH, "r")
+    lines = file.readlines()
+    for line in lines:
+        # Continue if not comment
+        if line[0] != "#":
+            info = line.rstrip().split("<SEP>")
+            # Continue if line has all 6 info fields
+            if len(info) == 6:
+                # Get mxm artist name from line
+                artist_name = info[4]
+                # Add to list
+                all_artists.append(artist_name)
+    file.close()
+    # Remove duplicates
+    all_artists = list(set(all_artists))
+
+    # Select names of solo artists 
+    all_artists_first_names = [name.split(" ")[0] for name in all_artists]
+    solo_artists_first_names = list(set(all_artists_first_names).intersection(names))
+    solo_artists = [artist for artist in all_artists if artist.split(" ")[0] in solo_artists_first_names]
+
+    # Write to file
+    # pd.DataFrame({"artists": solo_artists}).to_csv(OUTPUT_PATH, sep=",", encoding="utf-8")
+    with open(OUTPUT_PATH,"w") as f:
+        wr = csv.writer(f, delimiter="\n")
+        wr.writerow(solo_artists)
+
+
+
+
+    
