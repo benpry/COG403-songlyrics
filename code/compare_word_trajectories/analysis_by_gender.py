69193cc0f0be1a20fcaad054cb9b50a82fdbc393
Ria Prakash <riaprakash.1999@gmail.com>
2021-03-26 16:48:26 -0400

    Completed analysis by gender

--- code/compare_word_trajectories/analysis_by_gender.py
+++ code/compare_word_trajectories/analysis_by_gender.py
@@ -1,25 +1,24 @@
 """
-This does the analysis by genre
+This does the analysis by gender
 """
 from cross_correlations import get_best_offset, bootstrap_test
 import pandas as pd
 import numpy as np
 
-GENRES_FILEPATH = "../../data/processed/genres.csv"
-GENRE_DIRECTORY = "../../data/processed/freqs_by_genre"
+GENDER_DIRECTORY = "../../data/processed/freqs_by_gender"
 DATA_PATH = "../../data/processed"
 
 if __name__ == "__main__":
 
-    genres = list(pd.read_csv(GENRES_FILEPATH)["genre"].drop_duplicates())
+    genders = ['female', 'male']
 
     df_book = pd.read_csv(f"{DATA_PATH}/book_frequencies.csv")
     df_book = df_book.drop(["Corpus"], axis=1)
     df_book.columns = ["", "year", "word", "freq"]
     df_book = df_book[(df_book["year"] >= 1960) & (df_book["year"] <= 2011)]
 
-    for genre in genres:
-        df_song = pd.read_csv(f"{GENRE_DIRECTORY}/{genre}.csv")
+    for gender in genders:
+        df_song = pd.read_csv(f"{GENDER_DIRECTORY}/{gender}.csv")
         df_song = df_song[df_song["year"] >= 1960]
 
         best_offsets = []
@@ -30,5 +29,5 @@ if __name__ == "__main__":
         mean_offset = np.nanmean(best_offsets)
         p_value = bootstrap_test(mean_offset, best_offsets)
 
-        print(f"{genre} mean offset: {mean_offset}, p={p_value}")
+        print(f"{gender} mean offset: {mean_offset}, p={p_value}")
         print(f"median offset: {np.nanmedian(best_offsets)}")
